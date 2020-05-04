86c5d48b97c90f2cc1a3819a41c491f211265b62
JerryDenzinger <gerardo_denzinger@outlook.de>
2020-05-04 12:03:50 +0200

    To do list modified and add Help Documents

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,36 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({extenden :true}));
+app.use(bodyParser.urlencoded({ extenden: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-var items = [];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
-      weekday:"long",
-      day: "numeric",
-      month: "long"
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US" ,options);
+  let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newItems: items });
+  res.render("list", { listTitle: day, newItems: items });
 });
 
-
-
-
-app.post("/", function (req,res){
-    item = req.body.upNext;
+app.post("/", function (req, res) {
+  item = req.body.upNext;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
     items.push(item);
-
     res.redirect("/");
-    
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work Day", newItems: workItems });
+});
+
+app.get("/about" ,function(req,res){
+  res.render("about");
 });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
-

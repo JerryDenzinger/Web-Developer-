890fd72ff3ecb709ea010d2a685bde3f1e893b4f
JerryDenzinger <JerryDenzinger@users.noreply.github.com>
2021-02-17 11:20:46 +0100

    Fruits project using mongoDb and Mongoose

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,70 +1,59 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-//Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name 
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// Use connect method to connect to the Server 
-client.connect(function (err) {
-    assert.strictEqual(null, err);
-    console.log("Connected succesfully to server");
-
-    const db = client.db(dbName);
-
-    // insertDocuments(db, function () {
-    //   client.close;
-    //});
-    findDocuments(db, function () {
-        client.close;
-    });
+const fruitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name required !!! Please give complete Data"]
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 10
+    },
+    review: String
 });
 
-const insertDocuments = function (db, callback) {
-    // Get Fruits collection 
-    const collection = db.collection('fruits');
-    // Insert some documents 
-    collection.insertMany([
-        {
-            name: "Apple",
-            score: 8,
-            review: "Great fruit"
-        },
-        {
-            name: "Orange",
-            score: 6,
-            review: "Kind of sour"
-        },
-        {
-            name: "Banana",
-            score: 9,
-            review: "Great stuff!!"
-        }
-    ], function (err, result) {
-        assert.strictEqual(err, null);
-        assert.strictEqual(3, result.result.n);
-        assert.strictEqual(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-};
+// const fruit = new Fruit({
+//     name: "Melon",
+//     rating: 8,
+//     review: "So round fruit!!"
+// });
 
-const findDocuments = function (db, callback) {
-    // Get Fruits collection 
-    const collection = db.collection('fruits');
-    // find some documents 
-    collection.find({}).toArray(function (err, fruits) {
-        assert.strictEqual(err, null);
-        console.log("Found the following records");
-        console.log(fruits);
-        callback(fruits);
-    })
-};
+// fruit.save();
 
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    favouriteFruit: fruitSchema
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const grapes = new Fruit({
+    name: "Grapes",
+    rating: 10,
+    review: "Delicuis frish , or al rasins but most as Wine!!"
+});
+
+grapes.save();
+
+Person.updateOne({ name: "John" }, { favouriteFruit: grapes }, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Data succesfully updated!!!");
+    }
+});
+
+// const person = new Person({
+//     name: "Amy",
+//     age: 37,
+//     favouriteFruit: pineapple
+// });
+
+//person.save();
 

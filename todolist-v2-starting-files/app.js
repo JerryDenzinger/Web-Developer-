a6ed5934f8e686c2b7f3b4ed08b7691da9c7511d
JerryDenzinger <JerryDenzinger@users.noreply.github.com>
2021-04-19 15:14:46 +0200

    to do list v2

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://jerry-admin:Test123@cluster0.ohkni.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemsSchema = new mongoose.Schema({
   name: String
